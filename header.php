1d5206a30dc1d3f93677024e4259e103124c70df
TimilehinPromise <awoyeyetimilehin@gmail.com>
2021-12-01 15:27:07 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,85 @@
+<html>
+<body>
+<nav class="navbar navbar-expand-lg  bg-primary sticky-top">
+    <div class="container">
+      <a class="navbar-brand" href="">
+      </a>
+      <!-- Toggler for short width like phone or tablet -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" 
+      data-target="#navbarNavAltMarkup">
+        <span class="navbar-toggler-icon"> ||| </span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <!-- The start of the navbar item -->
+        <ul class="navbar-nav ml-auto" style="text-align: left">
+
+            <!-- first navbar item --> 
+            <div class="Navbar">
+              <button type="button" class="btn btn-primary">
+               <li><a style="color:#fefefe" href="universityindex.html">Home</a> </li>
+               </button>
+           </div>
+
+            <!-- second navbar item -->
+            <div class="Navbar">
+               <button type="button" class="btn btn-primary">
+                <li><a style="color:#fefefe" href="universityindex.html">About</a> </li>
+                </button>
+            </div>
+            <!-- Third navbar item -->
+            <div class="Navbar">
+              <button type="button" class="btn btn-primary">
+               <li><a style="color:#fefefe" href="registrationprocedure.html">Registration Procedure</a> </li>
+               </button>
+           </div>
+           <!-- Fourth navbar item -->
+           <div class="dropdown">
+            <button type="button" class="btn btn-primary dropdown-toggle" id="dropdownmenu" data-toggle="dropdown" aria-expanded="false">
+                Requirement<span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownmenu">
+                <li> <a class="dropdown-item" href="Document.html">Required Document</a></li>
+                <li> <a class="dropdown-item" href="steps.html">Step</a></li>
+            </ul>
+          </div>
+
+            <!-- Fifth navbar item -->
+            <div class="dropdown">
+                <button type="button" class="btn btn-primary dropdown-toggle" id="dropdownmenu" data-toggle="dropdown" aria-expanded="false">
+                    Department Requirement<span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownmenu">
+                    <li> <h6 class="dropdown-header">  Department</h6></li>
+                    <div class="dropdown-divider"></div>
+                    <li> <a class="dropdown-item" href="sosrequirement.html">SOS</a></li>
+                    <li> <a class="dropdown-item" href="smatrequirement.html">SMAT</a></li>
+                    <li> <a class="dropdown-item" href="setrequirement.html">SET</a></li>
+                    <li> <a class="dropdown-item" href="semsrequirement.html">SEMS</a></li>
+                    <li> <a class="dropdown-item" href="seetrequirement.html">SEET</a></li>
+                    <div class="dropdown-divider"></div>
+                    <li> <h6 class="dropdown-header"> General Requirement</h6></li>
+                    <li> <a class="dropdown-item" href="#">General Requirement</a></li>
+                </ul>
+                </div>
+                <!-- Sixth navbar item -->
+              <div class="dropdown">
+                <button type="button" class="btn btn-primary dropdown-toggle" id="dropdownmenu" data-toggle="dropdown" aria-expanded="false">
+                  Building<span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownmenu">
+                    <li> <a class="dropdown-item" href="MALEHOSTEL.HTML">Male Hostel</a></li>
+                    <li> <a class="dropdown-item" href="femalehostel.html">Female hostel</a></li>
+                    <li> <a class="dropdown-item" href="SEET.html">SEET</a></li>
+                    <li> <a class="dropdown-item" href="sos.html">SOS</a></li>
+                    <li> <a class="dropdown-item" href="saat.html">SAAT</a></li>
+                    <li> <a class="dropdown-item" href="SEEM.html">SEMS</a></li>
+                    <li> <a class="dropdown-item" href="set.html">SET</a></li>
+                </ul>
+                </div>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+</body>
